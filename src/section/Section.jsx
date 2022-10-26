9f0ec163687b4f04d79014c491d3d398193af4f5
Sinan <sinankzu@gmail.com>
2022-10-26 20:45:56 +0200

    aa

--- src/section/Section.jsx
+++ src/section/Section.jsx
@@ -5,8 +5,27 @@ const Section = () => {
   const palindrome = [];
   const [ilkk, setIlkk] = useState(20);
   const [sonn, setSonn] = useState(40);
+  const [perfect, setPerfect] = useState();
+  const liste =[]
     
-
+    // 28 % x == 0
+    // 1
+    // 2
+    // 4
+    // 7
+    // 14
+const deneme = () => {
+     for (let i = 0; i <= sonn - ilkk; i++){
+        setPerfect(i);
+        for (let x = 0; x <= sonn - ilkk; x++){
+            if (perfect < x && perfect % x === 0) {
+              liste.push(x);
+            }
+        }
+          
+     }
+    }
+    console.log(liste)
   const [salla, setSalla] = useState([]);
 
   const calcPal = () => {
@@ -40,7 +59,7 @@ const Section = () => {
       />
 
       <button onClick={calcPal}>List palindrome</button>
-      <button>List perfect number</button>
+      <button onClick={deneme}>List perfect number</button>
 
       <div className="text1">
         {salla.map((e) => (
